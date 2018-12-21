0bcd06d0c07fe4470b7b001444ea8624062de0d7
Aimee Zawacki <aimee@tna.digital>
2018-12-21 13:31:48 -0700

    updated readme and cleared out existing code.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Baby's First React App
 ### A Todo List for forgetful people.
 
+Your goal is to create a simple Todo List App that fulfills the user stories below. This project is meant to be open ended, so feel free to make it your own. Consider style, user experience, and feature branch workflow. 
+
 ### Specs
 - Use the corresponding Todos API 
   - You can use the deployed version [here.](https://galvanize-todos-api.herokuapp.com/) Keep in mind, todos you add and delete will be visible to everyone using this API. 
--- src/App.js
+++ src/App.js
@@ -1,61 +1,20 @@
 import React, { Component } from 'react';
-import AddListItem from './components/addlistitem.js'
-import List from './components/list.js'
-import todos from './data.js'
 
 class App extends Component {
 
   constructor() {
     super() 
     this.state = {
-      newTodo: "",
       todos: []
     }
   }
 
-  updateTodo = (event) => {
-    // console.log("value", event.target.value)
-    this.setState({
-      newTodo: event.target.value
-    }) 
-  }
-
-  componentDidMount(){
-    fetch('http://localhost:8000/')
-      .then(response => response.json())
-        .then(response => {
-          this.setState({todos: response})
-          console.log(response)
-          return response
-        })
-  }
-
-  addTodo = (event) => {
-    event.preventDefault()
-    if(this.state.newTodo.length === 0){
-      alert('NO TODO NO LIST')
-    } 
-    else {
-      let todo = {
-        todo: this.state.newTodo,
-      }
-      this.setState({
-        todos: [...this.state.todos, todo]
-      })
-  }
-  }
 
   render() {
     return (
-      <div>
-        <AddListItem 
-          updateTodo={this.updateTodo}
-          addTodo={this.addTodo}/>
-        <List 
-          todos={this.state.todos}/>
-      </div>
-    );
+      <div>Your Code Here</div>
+      )
   }
 }
 
-export default App;
+export default App
